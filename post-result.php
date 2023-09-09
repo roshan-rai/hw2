e845b6347df3621263b172ce8a3be052aea7d878
Roshan Rai <95188617+roshan-rai@users.noreply.github.com>
2023-09-09 13:41:13 -0500

    Update post-result.php
    fixed few errors for post-result php

--- post-result.php
+++ post-result.php
@@ -1,20 +1,15 @@
 <?php
 $pageTitle = "Post result";
 include "view-header.php";
-
 ?>
     <h1>Post result</h1>
   <?php
 echo getDisplay(); 
 include "view-footer.php";
 
-
 function getDisplay(){
     if (isset($_POST['my-name'])) {
-  
-  return "<p>The value sent is:</p>" $_POST['my-name'];
-  <?php
-  echo $_POST['my-name'];
+          return "<p>The value sent is:</p>" $_POST['my-name'];
    }
     else {
     return 
